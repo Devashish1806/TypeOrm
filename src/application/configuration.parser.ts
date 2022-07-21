b19ae2009fdd95e945be937557c94314ac2afbb6
Devashish Mali <devashish.mali06@gmail.com>
2022-07-21 17:49:10 +0530

    configuration parsed

---
+++ src/application/configuration.parser.ts
@@ -0,0 +1,49 @@
+import { readdirSync, readJSONSync } from 'fs-extra-promise';
+import { join } from 'path';
+import * as _ from 'lodash';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+
+export class ConfigurationParser {
+  private static __instance: ConfigurationParser = null;
+  private __baseResourcePath: string = null;
+  private __config: any = {};
+
+  private constructor() {
+    this.__baseResourcePath = join(process.cwd(), 'resources');
+    this.__parseConfig(this.__baseResourcePath);
+    console.log(JSON.stringify(this.__config));
+  }
+
+  static getInstance(): ConfigurationParser {
+    if (ConfigurationParser.__instance === null) {
+      ConfigurationParser.__instance = new ConfigurationParser();
+      Object.freeze(ConfigurationParser.__instance);
+    }
+    return ConfigurationParser.__instance;
+  }
+
+  private __parseConfig(resourceFolder: string) {
+    const configFiles: string[] = readdirSync(resourceFolder).filter((file) => {
+      return file.match(
+        new RegExp(`(\\w+\\.(?:config\\.(yaml|yml|json)))`, 'gi'),
+      );
+    });
+    configFiles.map((file) => {
+      let fileObject = {};
+      _.merge(fileObject, this.__parseFileObjects(join(resourceFolder, file)));
+      this.__config[
+        file
+          .split('.')[0]
+          .replace(/[\-\_]/gi, '')
+          .toLowerCase()
+      ] = fileObject;
+    });
+  }
+
+  private __parseFileObjects(file: string) {
+    if (file.endsWith('yml') || file.endsWith('yaml'))
+      return yaml.load(readFileSync(file, 'utf-8'));
+    else return readJSONSync(file);
+  }
+}
